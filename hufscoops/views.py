8802e4944302bb289348c55818707dd821367589
roharon <roharon@hufs.ac.kr>
2017-07-13 23:33:20 +0900

    DB 추가

--- hufscoops/views.py
+++ hufscoops/views.py
@@ -1,97 +1,309 @@
 from django.views.decorators.csrf import csrf_exempt
-import json, datetime
 from django.http import JsonResponse
-import requests
-import urllib.request
 from bs4 import BeautifulSoup
-
+import json, datetime, requests, time
+import urllib.request
+import sqlite3, os
 
 def keyboard(request):
+
     return JsonResponse({
         'type': 'buttons',
-        'buttons': ['후생관', '어문관', '기숙사 식당', '교직원 식당', '국제사회교육원', '도서관 좌석']
+        'buttons': ['학식', '도서관', '캠퍼스 변경']
     })
 
 
 @csrf_exempt
 def message(request):
+    button_info = ['학식', '도서관', '캠퍼스 변경']
+    user_type = 'NO'
+    start_time = time.time()
     today = datetime.date.today()
     today_date = today.strftime('%m월 %d일')
     json_str = (request.body).decode('utf-8')
     received_json_data = json.loads(json_str)
-    cafeteria_name = received_json_data['content']
+    content_name = received_json_data['content']
+    # content 의 내용
+    user_name = received_json_data['user_key']
+    # user_key 의 내용
+    print(user_name + '님이 ' + content_name + '을 눌렀습니다')
 
-    if cafeteria_name == '도서관 좌석':
+    con = sqlite3.connect("./DB/userdata.db")
+    cur = con.cursor()
+
+    # #cur.execute("CREATE TABLE user_data(Name TEXT, Campus TEXT);")
+    if "캠퍼스 변경" in content_name:
         return JsonResponse({
             'message': {
-                'text': '열람실을 선택하세요'
+                'text': '캠퍼스를 선택하세요'
             },
             'keyboard': {
                 'type': 'buttons',
-                'buttons': ['3층 1열람실: ' + str(library(1)[6]) + '%', '3층 2열람실: ' + str(library(2)[6]) + '%', '4층 3열람실A: ' + str(library(3)[6]) + '%',
-                            '4층 3열람실B: ' + str(library(4)[6]) + '%', '메인으로 가기']
+                'buttons': ['서울', '글로벌']
             }
-
         })
 
-    elif cafeteria_name == '메인으로 가기':
+    elif content_name in ("서울", "글로벌"):
+        print("작업중 1234")
+        cur.execute("DELETE FROM user_data WHERE Name = (:name)", {"name": user_name})
+        cur.execute("INSERT into user_data(Name, Campus) VALUES (?,?)", (user_name, content_name))
+        con.commit()
+        con.close()
         return JsonResponse({
             'message': {
-                'text': '메인으로 이동합니다'
+                'text': content_name+'캠퍼스를 선택하였습니다'
             },
             'keyboard': {
                 'type': 'buttons',
-                'buttons': ['후생관', '어문관', '기숙사 식당', '교직원 식당', '국제사회교육원', '도서관 좌석']
+                'buttons': button_info
             }
         })
 
-    elif ':' in cafeteria_name:
+    cur.execute("SELECT Campus from user_data WHERE Name = (:name)", {"name": user_name})
+    confirm = cur.fetchall()
 
-        if '3층 1열람실: ' in cafeteria_name:
-            name = "3층 1열람실 현황: "
-            lib_num = 1
-            room_no = 8
-        elif '3층 2열람실: ' in cafeteria_name:
-            name = "3층 2열람실 현황: "
-            lib_num = 2
-            room_no = 9
-        elif '4층 3열람실A: ' in cafeteria_name:
-            name = "4층 3열람실A 현황: "
-            lib_num = 3
-            room_no = 10
-        elif '4층 3열람실B: ' in cafeteria_name:
-            name = "4층 3열람실B 현황: "
-            lib_num = 4
-            room_no = 11
-
+    if len(confirm) == 0:
         return JsonResponse({
             'message': {
-                'text': name + str(library(lib_num)[6]) + '%' + '\n이용자 수: ' + str(library(lib_num)[4]) + '명    남은 좌석 수: ' + str(library(lib_num)[5]),
-                'message_button': {
-                    'label': '좌석보기',
-                    'url': 'http://203.232.237.8/domian5/2/roomview5.asp?room_no=' + str(room_no)
+                'text': '캠퍼스를 선택하세요'
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['서울', '글로벌']
+            }
+        })
+
+    cur.execute("SELECT Campus from user_data WHERE Name = (:name)", {"name": user_name})
+    data = cur.fetchone()
+
+    if "글로벌" in data:
+        user_type = 'global'
+        print("\n\n글로벌 유저\n")
+    elif "서울" in data:
+        user_type = 'seoul'
+        print("\n\n서울 유저\n")
+        #else:
+
+    #elif "캠퍼스 변경" in content_name:
+    con.commit()
+    con.close()
+
+    if "NO" in user_type:
+        return JsonResponse({
+            'message': {
+                'text': '최초 캠퍼스 설정 값이 없습니다\n캠퍼스를 골라주세요'
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['서울', '글로벌']
+            }
+        })
+
+
+    if content_name == '학식':
+        if user_type == 'global':
+            return JsonResponse({
+                'message': {
+                    'text': '글로벌 캠퍼스의 학식'
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                    'buttons': ['후생관', '어문관', '기숙사 식당', '교직원 식당', '국제사회교육원']
                 }
-            },
-            'keyboard': {
+            })
+        elif user_type == 'seoul':
+            return JsonResponse({
+                'message': {
+                    'text': '서울 캠퍼스의 학식'
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                    'buttons': ['인문관', '교수회관', '스카이 라운지']
+                }
+            })
+        else:
+            return JsonResponse({
+                'message': {
+                    'text': '학식 오류. 개발자에게 문의하세요'
+                },
                 'type': 'buttons',
-                'buttons': ['후생관', '어문관', '기숙사 식당', '교직원 식당', '국제사회교육원', '도서관 좌석']
-            }
-        })
+                'buttons': button_info
+            })
 
-    else:
+    elif content_name == '도서관':
+        if user_type == 'seoul':
+            lib_data = seo_library(content_name)
+            buttons = ['4층 제1열람실(A): ' + str(lib_data['4-1A'] + '%'),
+                    '4층 제1열람실(B): ' + str(lib_data['4-1B'] + '%'),
+                    '4층 제2열람실(노트북): ' + str(lib_data['4-2'] + '%'),
+                    '5층 제3열람실(A): ' + str(lib_data['5-3A'] + '%'),
+                    '5층 제3열람실(B): ' + str(lib_data['5-3B'] + '%'), '5층 제4열람실: ' + str(lib_data['5-4'] + '%')
+                        ]
+            end_time = time.time()
+            print(end_time - start_time)
+            return JsonResponse({
+                'message': {
+                    'text': '열람실을 선택하세요'
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                    'buttons': buttons
+                }
+
+            })
+        elif user_type == 'global':
+            lib_data = glo_library(content_name)
+            buttons = ['3층 1열람실: ' + str(lib_data['3-1']) + '%',
+                       '3층 2열람실: ' + str(lib_data['3-2']) + '%',
+                       '4층 3열람실A: ' + str(lib_data['4-3A']) + '%',
+                       '4층 3열람실B: ' + str(lib_data['4-3B']) + '%'
+                       ]
+
+            end_time = time.time()
+            print(end_time - start_time)
+            return JsonResponse({
+                'message': {
+                    'text': '열람실을 선택하세요'
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                    'buttons': buttons
+                }
+
+            })
+
+
+    elif '열람실' in content_name:
+        if user_type == 'seoul':
+            if '4층 제1열람실(A)' in content_name:
+                name = "4층 제1열람실(A) 현황: "
+                lib_num = 1
+                room_no = 1
+            elif '4층 제1열람실(B)' in content_name:
+                name = "4층 제1열람실(B) 현황: "
+                lib_num = 2
+                room_no = 2
+            elif '4층 제2열람실(노트북)' in content_name:
+                name = '4층 제2열람실(노트북) 현황: '
+                lib_num = 3
+                room_no = 3
+            elif '5층 제3열람실(A)' in content_name:
+                name = '5층 제3열람실(A) 현황: '
+                lib_num = 4
+                room_no = 4
+            elif '5층 제3열람실(B)' in content_name:
+                name = '5층 제3열람실(B) 현황: '
+                lib_num = 5
+                room_no = 5
+            elif '5층 제4열람실' in content_name:
+                name = '5층 제4열람실 현황: '
+                lib_num = 6
+                room_no = 6
+
+            lib_data = seo_library(lib_num)
+            end_time = time.time()
+            print(end_time - start_time)
+            return JsonResponse({
+                'message': {
+                    'text': name + str(lib_data['%']) + '%' + '\n이용자 수: ' + str(lib_data['이용자']) +
+                            '명\n남은 좌석 수: ' + str(lib_data['남은 좌석']),
+                    'message_button': {
+                        'label': '좌석보기',
+                        'url': 'http://203.232.237.8/domian5/roomview5.asp?room_no=' + str(room_no)
+                    }
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                    'buttons': button_info
+                }
+            })
+
+        elif user_type == 'global':
+            if '3층 1열람실: ' in content_name:
+                name = "3층 1열람실 현황: "
+                lib_num = 1
+                room_no = 8
+            elif '3층 2열람실: ' in content_name:
+                name = "3층 2열람실 현황: "
+                lib_num = 2
+                room_no = 9
+            elif '4층 3열람실A: ' in content_name:
+                name = "4층 3열람실A 현황: "
+                lib_num = 3
+                room_no = 10
+            elif '4층 3열람실B: ' in content_name:
+                name = "4층 3열람실B 현황: "
+                lib_num = 4
+                room_no = 11
+            lib_data = glo_library(lib_num)
+
+            end_time = time.time()
+            print(end_time - start_time)
+            return JsonResponse({
+                'message': {
+                    'text': name + str(lib_data['%']) + '%' + '\n이용자 수: ' + str(lib_data['이용자']) +
+                            '명\n남은 좌석 수: ' + str(lib_data['남은 좌석']),
+                    'message_button': {
+                        'label': '좌석보기',
+                        'url': 'http://203.232.237.8/domian5/roomview5.asp?room_no=' + str(room_no)
+                    }
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                    'buttons': button_info
+                }
+            })
+        else:
+            return JsonResponse({
+                'message': {
+                    'text': '도서관 오류. 개발자에게 문의하세요'
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                    'buttons': button_info
+                }
+            })
+
+    elif content_name in ('인문관', '교수회관', '스카이 라운지'):
+            text = str(today_date) + ' ' + content_name + '의 메뉴\n' + str(seo_crawl(content_name))
+            end_time = time.time()
+            print(end_time - start_time)
+            return JsonResponse({
+                'message': {
+                    'text': text
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                    'buttons': button_info
+                }
+            })
+
+    elif content_name in ('후생관', '어문관', '기숙사 식당', '교직원 식당', '국제사회교육원'):
+        text = str(today_date) + ' ' + content_name + '의 메뉴\n' + str(glo_crawl(content_name))
+        end_time = time.time()
+        print(end_time - start_time)
         return JsonResponse({
             'message': {
-                'text': str(today_date) + ' ' + cafeteria_name + '의 메뉴' + '\n' + str(crawl(cafeteria_name))
+                'text': text
                 },
             'keyboard': {
                 'type': 'buttons',
-                'buttons': ['후생관', '어문관', '기숙사 식당', '교직원 식당', '국제사회교육원', '도서관 좌석']
+                'buttons': button_info
             }
     })
+    else:
+        return JsonResponse({
+            'message': {
+                'text': "이걸 보고계시다면 오류입니다, 개발자에게 알려주세요"
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': button_info
+            }
+        })
 
-
-def crawl(cafeteria):
-
+def glo_crawl(cafeteria):
+    start = time.time()
     today = datetime.date.today()
     today_d = today.strftime("%Y%m%d")
 
@@ -121,7 +333,6 @@ def crawl(cafeteria):
         if len(data) == 1:
             cafe_menu.append(i)
             return "  오늘은 학식이 없어요!"
-
         else:
             if "\n" in i:
                 if "방학 중" in i:
@@ -139,7 +350,8 @@ def crawl(cafeteria):
 
     for size in range(1, menu_size):
         time_size = len(cafe_menu[size])
-        menu = menu + '\n----------------\n\n'
+
+        menu = menu + '\n----------------\n'
 
         for what in range(0, time_size):
             if what == (time_size-1):
@@ -152,12 +364,71 @@ def crawl(cafeteria):
             else:
                 menu = menu + cafe_menu[size][what] + '\n'
 
+    stop = time.time()
+    print(stop - start)
     return menu
 
 
+def seo_crawl(cafeteria):
+    start = time.time()
+    today = datetime.date.today()
+    today_d = today.strftime("%Y%m%d")
 
+    if cafeteria == '인문관':
+        req = requests.get('https://webs.hufs.ac.kr/jsp/HUFS/cafeteria/viewWeek.jsp?startDt=' + today_d + '&endDt=' + today_d + '&caf_name=%C0%CE%B9%AE%B0%FC%BD%C4%B4%E7&caf_id=h101')
+    elif cafeteria == '교수회관':
+        req = requests.get('https://webs.hufs.ac.kr/jsp/HUFS/cafeteria/viewWeek.jsp?startDt=' + today_d + '&endDt=' + today_d + '&caf_name=%B1%B3%BC%F6%C8%B8%B0%FC%BD%C4%B4%E7&caf_id=h102')
+    elif cafeteria == '스카이 라운지':
+        req = requests.get('https://webs.hufs.ac.kr/jsp/HUFS/cafeteria/viewWeek.jsp?startDt=' + today_d + '&endDt=' + today_d + '&caf_name=%BD%BA%C4%AB%C0%CC%B6%F3%BF%EE%C1%F6&caf_id=h103')
 
-def library(num):
+    html = req.text
+    soup = BeautifulSoup(html, 'lxml')
+    my_titles = soup.select(
+        'tr'
+    )
+    data = []
+    cafe_menu = []
+
+    for title in my_titles:
+        data.append(title.text)
+
+    for i in data:
+        if len(data) == 1:
+            cafe_menu.append(i)
+            # return "오늘은 학식이 없어요!"
+        else:
+            if "\n" in i:
+                i = i.replace('\n', ' ').replace('&', ' ').replace('*', ' ').split()
+
+                if '메뉴는' in i[1]:
+                    # 교수회관
+                    break
+                elif '닭강정-' in i[0]:
+                    # 인문관
+                    break
+                elif '※' in i[0]:
+                    # 스카이라운지
+                    break
+                cafe_menu.append(i)
+
+    menu_size = len(cafe_menu)
+    menu = ''
+
+    for size in range(1, menu_size):
+        time_size = len(cafe_menu[size])
+        menu = menu + '\n----------------\n'
+
+        for what in range(0, time_size):
+            if what == 0:
+                menu = menu + cafe_menu[size][what] + '\n\n'
+            elif cafe_menu[size][what].isdigit() == 1:
+                # Kcal앞 숫자의 경우에
+                menu = menu + cafe_menu[size][what]
+            else:
+                menu = menu + cafe_menu[size][what] + '\n'
+    return menu
+
+def glo_library(name):
     req = urllib.request.urlopen('http://203.232.237.8/domian5/2/domian5.asp')
     soup = BeautifulSoup(req, 'lxml', from_encoding="utf-8")
 
@@ -170,10 +441,48 @@ def library(num):
         data.append(title.text)
 
     # data[3]은 3층 1열람실 내용
-    return data[num+2].split()
+    # [3~7]까지의 (6)
+    # #return (data[num+2].split()
+    if name == "도서관":
+        return {'3-1': data[3].split()[6], '3-2': data[4].split()[6], '4-3A': data[5].split()[6], '4-3B': data[6].split()[6]}
+    else:
+        # #print(data[name+2].split())
+        return {'%': data[name+2].split()[6], '이용자': data[name+2].split()[4], '남은 좌석': data[name+2].split()[5]}
 
 
+def seo_library(name):
+    req = urllib.request.urlopen('http://203.232.237.8/domian5/domian5.asp')
+    soup = BeautifulSoup(req, 'lxml', from_encoding="utf-8")
 
+    my_titles = soup.select(
+        'tr'
+    )
+    data = []
+
+    for title in my_titles:
+        data.append(title.text)
+
+    # #return data[num+2].split()
+    if name == '도서관':
+        return {'4-1A': data[3].split()[6], '4-1B': data[4].split()[6], '4-2': data[5].split()[6], '5-3A': data[6].split()[6],
+                '5-3B': data[7].split()[6], '5-4': data[8].split()[6]}
+    else:
+        # #print(data[name+2].split())
+        return {'%': data[name+2].split()[6], '이용자': data[name+2].split()[4], '남은 좌석': data[name+2].split()[5]}
+"""
+@csrf_exempt
+def friend_add(request):
+    json_str = (request.body).decode('utf-8')
+    received_json_data = json.loads(json_str)
+    print('친구 등록 하였습니다' + received_json_data)
+
+
+@csrf_exempt
+def friend_remove(request):
+    json_str = (request.body).decode('utf-8')
+    received_json_data = json.loads(json_str)
+    print('친구 삭제 하였습니다' + received_json_data)
+"""
 
 
 
