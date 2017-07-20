a5f5010978620234d36c002fc5fd6db912fb18d6
roharon <roharon@hufs.ac.kr>
2017-07-21 00:27:19 +0900

    Update settings.py

--- hufscoops_project/settings.py
+++ hufscoops_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fw)$6w3y#9ao7q6uow@$z1iy4fu0byj8t012etq)a2m&!)(r__'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
