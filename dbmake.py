48ea2f26a32169d1ade7d294bff737122f788578
roharon <roharon@hufs.ac.kr>
2017-08-03 23:39:03 +0900

    Delete dbmake.py

--- dbmake.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("./DB/userdata.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE user_data(Name TEXT, Campus TEXT);")
-
-
-print("DataBase | CREATE TABLE, DONE.")
