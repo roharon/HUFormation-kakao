e0aa148f1c279f3a4cbd271b129850050fe16fd8
roharon <roharon98@gmail.com>
2019-04-19 22:28:59 +0900

    feat : 도서관 새 웹사이트에서 정보가져오기

--- hufscoops/library_crawl.py
+++ hufscoops/library_crawl.py
@@ -1,11 +1,12 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+get_percent = lambda x : int(int(x.split()[-2])/int(x.split()[-3]) * 100)
 
 def glo_library(name):
 
     try:
-        req = urllib.request.urlopen('http://203.232.237.8/domian5/2/domian5.asp')
+        req = urllib.request.urlopen('http://library.hufs.ac.kr:8091/mobile_new/lib_studyroom_state.asp')
     except:
         return 555
 
@@ -23,16 +24,21 @@ def glo_library(name):
     # [3~7]까지의 (6)
     # #return (data[num+2].split()
     if name == "도서관":
-        return {'3-1': data[3].split()[6], '3-2': data[4].split()[6], '4-3A': data[5].split()[6], '4-3B': data[6].split()[6]}
+        return {'3-1': get_percent(data[3]), 
+        '3-2': get_percent(data[4]), '4-3A': get_percent(data[5]), '4-3B': get_percent(data[6])
+        }
     else:
+        name += 2
+        #print(data[3].split())
+        percent = get_percent(data[name])
         # #print(data[name+2].split())
-        return {'%': data[name+2].split()[6], '이용자': data[name+2].split()[4], '남은 좌석': data[name+2].split()[5]}
+        return {'%': percent, '이용자': data[name].split()[-2], '남은 좌석': data[name].split()[-1] }
 
 
 def seo_library(name):
 
     try:
-        req = urllib.request.urlopen('http://203.232.237.8/domian5/domian5.asp')
+        req = urllib.request.urlopen('http://library.hufs.ac.kr:8091/mobile_new/lib_studyroom_state.asp')
     except:
         return 555
 
@@ -45,17 +51,19 @@ def seo_library(name):
 
     for title in my_titles:
         data.append(title.text)
-    print(data)
+    #print(data)
     # #return data[num+2].split()
 
     # 서울캠 도서관 임시도서관 열람실
 
+    #print(data[1].split())
     if name == "도서관":
-        return {'room-A' : data[3].split()[5],
-                'room-B' : data[4].split()[5],
+        return {'room-A' : get_percent(data[1]),
+                'room-B' : get_percent(data[2]),
                 }
     else:
-        return {'%': data[name+2].split()[5], '이용자': data[name+2].split()[3], '남은 좌석': data[name+2].split()[4] }
+        percent = get_percent(data[name])
+        return {'%': percent, '이용자': data[name].split()[-2], '남은 좌석': data[name].split()[-1] }
         # print(data[name+2].split())
 
 
@@ -72,4 +80,6 @@ def seo_library(name):
 
 
 if __name__ == "__main__":
-    print(seo_library("도서관"))
+    print(seo_library(2))
+    print(glo_library(1))
+    print(glo_library(2))
